f4964b9a4938bfa3a34c501307be7cd974966e9a
ian mcbrayer <mcbrayer.ian@gmail.com>
2021-03-10 19:46:41 -0500

    transfered script from index.js to app.js and deleted index.js

--- app.js
+++ app.js
@@ -12,6 +12,145 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
 
+const team = [];
+
+
+const managerQuestions = [
+    {
+        type: 'input',
+        name: 'managerName',
+        message: "Please enter your team manager's name.",
+    },
+    {
+        type: 'input',
+        name: 'managerID',
+        message: "Please enter your team manager's employee ID number.",
+    },
+    {
+        type: 'input',
+        name: 'managerEmail',
+        message: "Please enter your team manager's email address.",
+    },
+    {
+        type: 'input',
+        name: 'managerOfficeNumber',
+        message: "Please enter your team manager's office number.",
+    },
+]
+
+const engineerQuestions = [
+    {
+        type: 'input',
+        name: 'engineerName',
+        message: "Please enter your engineer's name.",
+    },
+    {
+        type: 'input',
+        name: 'engineerID',
+        message: "Please enter your engineer's ID number.",
+    },
+    {
+        type: 'input',
+        name: 'engineerEmail',
+        message: "Please enter your engineer's email address.",
+    },
+    {
+        type: 'input',
+        name: 'gitHub',
+        message: "Please enter your engineer's GitHub username.",
+    },
+]
+
+const internQuestions = [
+    {
+        type: 'input',
+        name: 'internName',
+        message: "Please enter your intern's name.",
+    },
+    {
+        type: 'input',
+        name: 'internID',
+        message: "Please enter your intern's ID number.",
+    },
+    {
+        type: 'input',
+        name: 'internEmail',
+        message: "Please enter your intern's email address.",
+    },
+    {
+        type: 'input',
+        name: 'internSchool',
+        message: "Please enter your intern's school.",
+    },
+]
+
+const menuQuestion = [
+    {
+        type: 'rawlist',
+        name: 'menu',
+        message: "Would you like to add an engineer or intern to your team?",
+        choices: [
+            'Engineer',
+            'Intern',
+            'No, team is complete.'
+        ]
+    }
+]
+
+function addManager() {
+    inquirer.prompt(managerQuestions) 
+    .then(data => {
+        const manager = new Manager(data.managerName, data.managerID, data.managerEmail, data.managerOfficeNumber);
+        team.push(manager);
+        addTeamMember();
+    })
+}
+
+addManager();
+
+function addTeamMember() {
+    inquirer.prompt(menuQuestion)
+    .then(data => {
+        switch(data.menu) {
+            case 'Engineer':
+                addEngineer();
+                break;
+            case 'Intern':
+                addIntern();
+                break;
+            default:
+                buildHTML();
+        }
+    })
+}
+
+function addEngineer() {
+    inquirer.prompt(engineerQuestions)
+    .then(data => {
+        const engineer = new Engineer(data.engineerName, data.engineerID, data.engineerEmail, data.gitHub);
+        team.push(engineer);
+        addTeamMember();
+    });
+
+}
+
+function addIntern() {
+    inquirer.prompt(internQuestions)
+    .then(data => {
+        const intern = new Intern(data.internName, data.internID, data.internEmail, data.internSchool);
+        team.push(intern);
+        addTeamMember();
+    });
+}
+
+function buildHTML() {
+    if (!fs.existsSync(path.resolve(__dirname, 'HTML'))) {
+        fs.mkdirSync(path.resolve(__dirname, 'HTML'));
+    }
+    fs.writeFileSync(path.join(path.resolve(__dirname, 'HTML'), 'index.html'), render(team), 'utf-8');
+}
+
+
 
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
